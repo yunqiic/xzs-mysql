55c7b6d3ce0481110a6ebadd2ba279cf9739593b
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-02 18:37:36 +0800

    update

--- source/wx/xzs-student/app.js
+++ source/wx/xzs-student/app.js
@@ -5,7 +5,7 @@ const mtjwxsdk = require('./utils/mtj-wx-sdk.js');
 
 App({
   globalData: {
-    baseAPI: "http://localhost:8000",
+    baseAPI: "http://82.157.54.206:8010/",
     pageSize: 20
   },
   onLaunch: function() {
